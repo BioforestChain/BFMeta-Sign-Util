132ca6899225f3a501835380e407c8004009e6bd
wzzzzx <349094287@qq.com>
2023-03-17 17:53:23 +0800

    update

--- src/encoding-utf8/text.node.ts
+++ src/encoding-utf8/text.node.ts
@@ -1,10 +1,1 @@
-import { TextDecoder, TextEncoder } from "util";
-if ("TextEncoder" in globalThis) {
-  if (TextEncoder) {
-    (globalThis as any).TextEncoder = TextEncoder;
-  }
-  if (TextDecoder) {
-    (globalThis as any).TextDecoder = TextDecoder;
-  }
-}
 export default () => ({ TextDecoder, TextEncoder });
